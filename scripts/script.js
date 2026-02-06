a09efe9e9ea4a47fc1096768b0ca949d343d7384
Orange <90908765+orangeZSCB@users.noreply.github.com>
2026-02-06 15:05:56 +0800

    fdas

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+// Typed.js
+  const options = {
+    strings: [
+          "是猫弦🐱",
+          "来自中国🐉",
+          "是一个小宅女 🏳️‍⚧️",
+          "是一个 FW 中学生🤓",
+          "是一个啥也不会的蒟蒻大佬横跳玛卡巴卡🤔",
+          "是......?"
+    ],
+    typeSpeed: 100,
+    loop: true,
+    startDelay: 500,
+  };
+
+  const typed = new Typed('#typed', options);
+
+// 随机年龄
+// 使用 Fitten Code 人工智能编写
+document.addEventListener('DOMContentLoaded', function () {
+  let timer;  // 声明一个变量来保存定时器ID
+  function updateRandomAge() {
+    var randomAge = Math.floor(Math.random() * 90) + 10;
+    document.getElementById("randomAge").textContent = randomAge;
+    clearTimeout(timer);  // 清除之前的定时器
+    timer = setTimeout(updateRandomAge, 50);  // 更新定时器
+  }
+
+  updateRandomAge();  // 首次调用
+});
